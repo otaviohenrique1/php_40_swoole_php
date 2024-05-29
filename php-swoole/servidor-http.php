d16adf12f7499c6acfded1a4e5a0c029de9bdbdf
Otavio Henrique de Lima e Silva <otaviohlsilva@gmail.com>
2024-05-29 18:55:31 -0300

    commit 3

--- php-swoole/servidor-http.php
+++ php-swoole/servidor-http.php
@@ -5,7 +5,8 @@ require_once 'vendor/autoload.php';
 use Swoole\Http\Server;
 use Swoole\Http\Request;
 use Swoole\Http\Response;
-use Swoole\Coroutine\Http\Client;
+
+CO::set(['hook_flags' => SWOOLE_HOOK_ALL]);
 
 $servidor = new Server("localhost", 8080);
 
@@ -13,19 +14,24 @@ $servidor->on('request', function(Request $request, Response $response) {
   // var_dump($request->get);
   // $response->header('Content-Type', 'text/html; charset=utf-8');
   // $response->end(printf($request->header, true));
-  go(function() {
-    $cliente = new Client('localhost', 8001);
-    $cliente->get('/servidor.php');
-    $conteudo = $cliente->getBody();
+  $channel = new chan(2);
+
+  go(function() use ($channel) {
+    $curl = curl_init('http://localhost:8001/servidor.php');
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    
+    $conteudo = curl_exec($curl);
+    $channel->push($conteudo);
   });
 
-  go(function() {
+  go(function() use ($channel) {
     $conteudo = file_get_contents('arquivo.txt');
+    $channel->push($conteudo);
   });
 
-  go(function() use (&$response) {
-    $primeiraResposta = '';
-    $segundaResposta = '';
+  go(function() use ($channel, &$response) {
+    $primeiraResposta = $channel.pop();
+    $segundaResposta = $channel.pop();
     $response->end($primeiraResposta . $segundaResposta);
   });
 });
